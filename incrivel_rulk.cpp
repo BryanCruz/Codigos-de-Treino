ac36248663ddc37b9260632a1f964b07d0fbca6c
Bryan <bryan.bialokur@gmail.com>
2017-11-16 20:31:14 +0000

    explicação do exercício

--- incrivel_rulk.cpp
+++ incrivel_rulk.cpp
@@ -1,53 +1,69 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+
+/*
+Rulk soca um muro de blocos e causa dano a ele, 
+sendo que quanto mais longe do soco, menos dano 
+o bloco sofre.
+Cada bloco (item da matriz) tem um grau de
+quão destruído está, sendo os valores iniciais
+definidos pelo usuário.
+
+Exemplo:
+
+Entrada: m = 4, n = 4 (tamanho da matriz)
+         x = 2, y = 2
+
+matriz = 10 10 10 10
+         10 10 10 10
+         10 10 10 10
+         10 10 10 10
+         
+Saída =  19 19 19 18
+         19 20 19 18
+         19 19 19 18
+         18 18 18 18
+*/
+
 int main(){
     int c, m, n, x, y;
-    //cin >> c;//numero de testes
-   // array matrizes[c];
-   
-  // cin>> c;
-    //testes
- //   for (int i = 0; i < c; i++) {
-        
-        //leitura e criacao do vetor
-        cin >> m >> n >> x >> y;
-        x--;
-        y--;
-        int matriz[m][n];
-        for (int j = 0; j < m; j++) {
-            for (int k = 0; k < n; k++) {
-                cin >> matriz[j][k];
-            }
+    cin >> m >> n >> x >> y;
+    x--;
+    y--;
+    int matriz[m][n];
+    for (int j = 0; j < m; j++) {
+        for (int k = 0; k < n; k++) {
+            cin >> matriz[j][k];
         }
+    }
 
-        int difM,difN, maior;
-        for (int j = 0; j < m; j++) {
-            for (int k = 0; k < n; k++) {
-                difM = x - j;
-                difN = y - k;
-                if(abs(difM)>abs(difN)){
-                    maior = abs(difM);
-                }else{
-                    maior = abs(difN);
-                }
-                
-                if(maior < 10){
-                    matriz[j][k] += 10 - abs(maior);
-                }else{
-                    matriz[j][k] += 1;
-                }
+    int difM,difN, maior;
+    for (int j = 0; j < m; j++) {
+        for (int k = 0; k < n; k++) {
+            difM = x - j;
+            difN = y - k;
+            if(abs(difM)>abs(difN)){
+                maior = abs(difM);
+            }else{
+                maior = abs(difN);
+            }
+            
+            if(maior < 10){
+                matriz[j][k] += 10 - abs(maior);
+            }else{
+                matriz[j][k] += 1;
             }
         }
-        //matri
-        
-        //exibicao
-       // for (int i = 1; i <= c; i++) {
-            cout << "Parede " << 1 << " :" << endl;
-            for (int j = 0; j < m; j++) {
-                for (int k = 0; k < n-1; k++) {
-                    cout << matriz[j][k] << " ";
-                }
-                cout << matriz[j][n-1] << endl;
-            }
-       // }
-//    }
-   // }
+    }
+
+    //exibicao
+    cout << "Parede " << 1 << " :" << endl;
+    for (int j = 0; j < m; j++) {
+        for (int k = 0; k < n-1; k++) {
+            cout << matriz[j][k] << " ";
+        }
+        cout << matriz[j][n-1] << endl;
+    }
 }
