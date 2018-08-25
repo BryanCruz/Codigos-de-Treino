c81c143d299f77ceb4849f812ede351b5a1c5ed2
BryanCruz <bryan.bialokur@gmail.com>
2018-08-25 03:28:25 -0300

    solved

---
+++ sbc-2017/README.md
@@ -0,0 +1,5 @@
+# Maratona Regional de SBC 2017
+Legenda:
+a1,..,an = arquivos de output
+i1,..,in = arquivos de input
+g1,..,gn = gabarito
--- sbc-2017/ginastica/main.cpp
+++ sbc-2017/ginastica/main.cpp
@@ -1,22 +1,47 @@
-#include<iostream>
-
 // https://www.urionlinejudge.com.br/judge/pt/problems/view/2664
 
-int quantity(int actual, int counter, int min, int max) {
-	if(actual < min || actual > max) return 0;
-	if(counter == 1) return 1;
-	return (quantity(counter-1, actual-1, min, max) + quantity(counter-1, actual+1, min, max)) % (10e9 + 7);
+#include<iostream>
+
+#define TMAX 51
+#define VMAX 100001
+
+using namespace std;
+
+// MATRIZ[TEMPO_INICIAL][V_INICIAL]
+int saveQtt[TMAX][VMAX];
+
+void initializeMatrix(int T, int min, int max){
+	for(int i = 0; i < TMAX; i++){
+		for(int j = 0; j < VMAX; j++){
+			saveQtt[i][j] = 0;
+		}
+	}
+
+	for(int i = min; i <= max; i++){
+		saveQtt[1][i] = 1;
+	}
 }
 
 int main(void){
+
 	int T, M, N;
 	cin >> T >> M >> N;
+	
+	initializeMatrix(T, M, N);
+	
+	for(int j = 2; j <= T; j++){
+		for(int i = M; i <= N; i++){
+			int v = 0;
+			v += saveQtt[j-1][i-1];
+			v += saveQtt[j-1][i+1];
+			saveQtt[j][i] = v % 1000000007;
+		}
+	}
 
 	int total = 0;
 	for(int i = M; i <= N; i++){
-		total += (quantity(i, T, M, N) % (10e9 + 7)); 
+		total = (total + saveQtt[T][i]) % 1000000007;
 	}
-
 	cout << total << endl;
 
 	return 0;
